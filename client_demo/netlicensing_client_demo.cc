bc6658dcc4fb0324af9e4260cf89ee8e70c3d0ae
Slava Rudkovskiy <xraskolx@gmail.com>
2017-09-15 13:14:46 +0300

    FIx netlicensing_client_demo

--- client_demo/netlicensing_client_demo.cc
+++ client_demo/netlicensing_client_demo.cc
@@ -23,23 +23,25 @@ int main(int argc, char* argv[]) {
 	try {
 		Context ctx;
 		ctx.set_base_url("https://go.netlicensing.io/core/v2/rest/");
+		std::cout << "NetLicensing URL: https://go.netlicensing.io/core/v2/rest/" << std::endl;;
 		ctx.set_username("demo");
+		std::cout << "Account:demo" << std::endl;;
 		ctx.set_password("demo");
 
 		if (licensee_number.empty()) {
-			std::cout << "Please enter a valid licensee number for validation (choose licensee in demo account): " << std::endl;
-			if (!std::getline(std::cin, licensee_number)) { return -1; }
+			do
+			{
+				std::cout << "Please enter a valid licensee number for validation (choose licensee in demo account): " << std::endl;
+				std::getline(std::cin, licensee_number);
 
-			if (!licensee_number.empty()) {
-				std::cout << "start validation for " << licensee_number << std::endl;
-				ValidationResult vres = LicenseeService::validate(ctx, licensee_number);
-				std::cout << "got validation results:\n" << vres.toString() << std::endl;
-			}
-			else {
-				std::cout << "Invalid licensee number for validation." << std::endl;
-				return -1;
-			}
+			} while (licensee_number.empty());
 
+			std::cout << "start validation for " << licensee_number << std::endl;
+			ValidationResult vres = LicenseeService::validate(ctx, licensee_number);
+			std::cout << "got validation results:\n" << vres.toString() << std::endl;
+
+			std::cout << "Please press enter key for exit"  << std::endl;
+			std::cin.ignore();
 		}
 	}
 	catch (const RestException& e) {
@@ -47,11 +49,13 @@ int main(int argc, char* argv[]) {
 		for (auto det : e.get_details()) {
 			std::cerr << det.to_string() << std::endl;
 		}
+		std::cout << "Please press enter key for exit"  << std::endl;
 		std::cin.ignore();
 		return 2;
 	}
 	catch (const std::runtime_error& err) {
 		std::cerr << err.what() << std::endl;
+		std::cout << "Please press enter key for exit"  << std::endl;
 		std::cin.ignore();
 		return 1;
 	}
