44701721fec366e51b3a7daaa1d2a49b87b3d2f3
Slava Rudkovskiy <xraskolx@gmail.com>
2017-09-15 11:51:07 +0300

    Add discounts delete functionality

--- client_demo/netlicensing_client_demo.cc
+++ client_demo/netlicensing_client_demo.cc
@@ -6,63 +6,101 @@
 #include "netlicensing/constants.h"
 
 int main(int argc, char* argv[]) {
-  using namespace netlicensing;
+	using namespace netlicensing;
 
-  std::string licensee_number = "IR2Q7A5P3";
-  if (argc > 1) {
-    licensee_number = argv[1];
-  }
+	std::string licensee_number = "IR2Q7A5P3";
+	if (argc > 1) {
+		licensee_number = argv[1];
+	}
 
-  std::mt19937 gen;
-  gen.seed(time(0));
-  std::stringstream ss;
-  ss << "P" << gen();
-  std::string productNumber = ss.str();
+	std::mt19937 gen;
+	gen.seed(time(0));
+	std::stringstream ss;
+	ss << "P" << gen();
+	std::string productNumber = ss.str();
 
-  std::cout << "Hello, this is NetLicensing demo client\n";
-  std::cout << "Product endpoint " << endpoint<Product>() << std::endl;
-  std::cout << "Product test number " << productNumber << std::endl;
+	std::cout << "Hello, this is NetLicensing demo client\n";
+	std::cout << "Product endpoint " << endpoint<Product>() << std::endl;
+	std::cout << "Product test number " << productNumber << std::endl;
 
-  try {
-    Context ctx;
-    ctx.set_base_url("https://go.netlicensing.io/core/v2/rest/");
-    ctx.set_username("demo");
-    ctx.set_password("demo");
+	try {
+		Context ctx;
+		ctx.set_base_url("https://go.netlicensing.io/core/v2/rest/");
+		ctx.set_username("demo");
+		ctx.set_password("demo");
 
-    // product section
-    Product p;
-    p.setName("Test name");
-    p.setNumber(productNumber);
-    Product newp = ProductService::create(ctx, p);
+		// product section
+		Product p;
+		p.setName("Test name");
+		p.setNumber(productNumber);
 
-    newp.setName("Updated name");
-    Product newp2 = ProductService::update(ctx, newp.getNumber(), newp);
+		std::list<ProductDiscount> discounts;
+		ProductDiscount discount;
+		ProductDiscount discount2;
 
-    std::list<Product> products = ProductService::list(ctx, "");
-    std::cout << "before delete products count " << products.size() << std::endl;
+		discount.setTotalPrice("20");
+		discount.setCurrency("EUR");
+		discount.setAmountFix("10");
 
-    ProductService::del(ctx, newp2.getNumber(), false);
+		discount2.setTotalPrice("25");
+		discount2.setCurrency("EUR");
+		discount2.setAmountPercent("10");
 
-    products = ProductService::list(ctx, "");
-    std::cout << "after delete products count " << products.size() << std::endl;
-    
-    if (!licensee_number.empty()) {
-      std::cout << "start validation for " << licensee_number << std::endl;
-      ValidationResult vres = LicenseeService::validate(ctx, licensee_number);
-      std::cout << "got validation results:\n" << vres.toString() << std::endl;
-    }
-  }
-  catch (const RestException& e) {
-    std::cerr << e.what() << " code " << e.http_code() << std::endl;
-    for (auto det : e.get_details()) {
-      std::cerr << det.to_string() << std::endl;
-    }
-    return 2;
-  }
-  catch (const std::runtime_error& err) {
-    std::cerr << err.what() << std::endl;
-    return 1;
-  }
+		discounts.push_back(discount);
+		discounts.push_back(discount2);
 
-  return 0;
+		p.setProductsDiscounts(discounts);
+
+		Product newp = ProductService::create(ctx, p);
+
+		std::list<ProductDiscount> newpDiscounts = newp.getDiscounts();
+
+		std::cout << "product disounts size: " << newpDiscounts.size() << std::endl;
+
+		int i = 0;
+		for (ProductDiscount newpDiscount : newpDiscounts) {
+			std::string newpDiscountStr = newpDiscount.toString();
+			std::cout << "product disount #" << i << ":" << newpDiscountStr << std::endl;
+			i++;
+		}
+
+		newp.setName("Updated name");
+
+		std::list<ProductDiscount> discountsEmpty;
+		newp.setProductsDiscounts(discountsEmpty);
+		std::cin.ignore();
+		Product newp2 = ProductService::update(ctx, newp.getNumber(), newp);
+
+		std::list<ProductDiscount> newp2Discounts = newp2.getDiscounts();
+
+		std::cout << "product disounts size " << newp2Discounts.size() << std::endl;
+		std::cin.ignore();
+		std::list<Product> products = ProductService::list(ctx, "");
+		std::cout << "before delete products count " << products.size() << std::endl;
+
+		ProductService::del(ctx, newp2.getNumber(), false);
+
+		products = ProductService::list(ctx, "");
+		std::cout << "after delete products count " << products.size() << std::endl;
+
+		if (!licensee_number.empty()) {
+			std::cout << "start validation for " << licensee_number << std::endl;
+			ValidationResult vres = LicenseeService::validate(ctx, licensee_number);
+			std::cout << "got validation results:\n" << vres.toString() << std::endl;
+		}
+	}
+	catch (const RestException& e) {
+		std::cerr << e.what() << " code " << e.http_code() << std::endl;
+		for (auto det : e.get_details()) {
+			std::cerr << det.to_string() << std::endl;
+		}
+		return 2;
+	}
+	catch (const std::runtime_error& err) {
+		std::cerr << err.what() << std::endl;
+		std::cin.ignore();
+		return 1;
+	}
+
+	return 0;
 }
--- include/netlicensing/converters.h
+++ include/netlicensing/converters.h
@@ -9,43 +9,49 @@
 
 namespace netlicensing {
 
-  template<typename T>
-  parameters_type toParametersList(T value);
+	template<typename T>
+	parameters_type toParametersList(T value);
 
-  template<>
-  inline parameters_type toParametersList<BaseEntity>(BaseEntity value) {
-    parameters_type params;
-    params.push_back(std::make_pair(NUMBER, value.getNumber()));
-    params.push_back(std::make_pair(ACTIVE, value.getActive().toString()));
-    for (const auto& prop : value.getProperties()) {
-      params.push_back(prop);
-    }
-    return params;
-  }
+	template<>
+	inline parameters_type toParametersList<BaseEntity>(BaseEntity value) {
+		parameters_type params;
+		params.push_back(std::make_pair(NUMBER, value.getNumber()));
+		params.push_back(std::make_pair(ACTIVE, value.getActive().toString()));
+		for (const auto& prop : value.getProperties()) {
+			params.push_back(prop);
+		}
+		return params;
+	}
 
-  template<>
-  inline parameters_type toParametersList<Product>(Product value) {
-    parameters_type params = toParametersList<BaseEntity>(value);
-    params.push_back(std::make_pair(NAME, value.getName()));
-    params.push_back(std::make_pair(VERSION, value.getVersion()));
-    params.push_back(std::make_pair(LICENSEE_AUTOCREATE, value.getLicenseeAutoCreate().toString()));
-    params.push_back(std::make_pair(DESCRIPTION, value.getDescription()));
-    params.push_back(std::make_pair(LICENSING_INFO, value.getLicensingInfo()));
-    // TODO(2K): convert discounts
-    /*
-    for (const auto& discount : value.getDiscounts()) {
-      // ...
-    }
-    */
-    return params;
-  }
+	template<>
+	inline parameters_type toParametersList<Product>(Product value) {
+		parameters_type params = toParametersList<BaseEntity>(value);
+		params.push_back(std::make_pair(NAME, value.getName()));
+		params.push_back(std::make_pair(VERSION, value.getVersion()));
+		params.push_back(std::make_pair(LICENSEE_AUTOCREATE, value.getLicenseeAutoCreate().toString()));
+		params.push_back(std::make_pair(DESCRIPTION, value.getDescription()));
+		params.push_back(std::make_pair(LICENSING_INFO, value.getLicensingInfo()));
 
-  template<>
-  inline parameters_type toParametersList<Licensee>(Licensee value) {
-    parameters_type params = toParametersList<BaseEntity>(value);
-    params.push_back(std::make_pair(NAME, value.getName()));
-    return params;
-  }
+		if (!value.getDiscounts().empty()) {
+			for (ProductDiscount discount : value.getDiscounts()) {
+				params.push_back(std::make_pair(DISCOUNT, discount.toString()));
+			}
+		}
+		else {
+			if (value.isProductDiscountsToched()) {
+				params.push_back(std::make_pair(DISCOUNT, ""));
+			}
+		}
+
+		return params;
+	}
+
+	template<>
+	inline parameters_type toParametersList<Licensee>(Licensee value) {
+		parameters_type params = toParametersList<BaseEntity>(value);
+		params.push_back(std::make_pair(NAME, value.getName()));
+		return params;
+	}
 
 }  // namespace netlicensing
 
